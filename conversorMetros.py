6d6f900b197180c14c4ec86ead77c7a3f215b847
Milena de Souza Rodrigues <110957438+RodrigsMi@users.noreply.github.com>
2022-11-03 13:44:04 -0300

    Create conversorMetros.py

---
+++ conversorMetros.py
@@ -0,0 +1,5 @@
+# CONVERTENDO METROS EM CM
+metros = int(input("Informe a quantidade de metros a ser convertida: "))
+centimetros = metros * 100
+ 
+print(f"{metros} metros se tornam {centimetros} em centímetros.")
