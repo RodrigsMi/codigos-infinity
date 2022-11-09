b4bcf687fa6c628a79115bcb78db604ee35ece25
Milena de Souza Rodrigues <110957438+RodrigsMi@users.noreply.github.com>
2022-11-09 10:01:21 -0300

    Create exercicioCondicional1.py
    1 - Faça um programa que receba dois números e mostre na tela qual é o maior, ou se são iguais.

---
+++ exercicioCondicional1.py
@@ -0,0 +1,9 @@
+num1 = input("Informe o primeiro número: ")
+num2 = input("Informe o segundo número: ")
+
+if num1>num2:
+    print(f"O número {num1} é maior")
+elif num2>num1:
+    print(f"O número {num2} é maior")
+else:
+    print("Os dois são iguais")
